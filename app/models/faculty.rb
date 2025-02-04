e58936c6c2d28a0691d715c04a898689de15dec9
James <jamesle2@andrew.cmu.edu>
2025-02-04 15:37:46 -0500

    Completed validations and test cases

--- app/models/faculty.rb
+++ app/models/faculty.rb
@@ -5,11 +5,10 @@ class Faculty < ApplicationRecord
     has_many :courses, through: :assignments
   
     # Scopes
-    # 1. `alphabetical` [order]
-    # 2. `active`       [where]
+    scope :alphabetical, -> { order(:last_name, :first_name) }
+    scope :active, -> { where(active: true) }
   
     # Validations
-    # 1. must have first, last names
-    # 2. rank must be either `Assistant Professor`, `Associate Professor`, or `Professor`
-  
+    validates_presence_of :first_name, :last_name
+    validates_inclusion_of :rank, in: ['Assistant Professor', 'Associate Professor', 'Professor']
 end
--- test/models/faculty_test.rb
+++ test/models/faculty_test.rb
@@ -2,20 +2,29 @@ require "test_helper"
 
 class FacultyTest < ActiveSupport::TestCase
   # Matchers
-
-
+  should validate_presence_of(:first_name)
+  should validate_presence_of(:last_name)
+  should validate_inclusion_of(:rank).in_array(['Assistant Professor', 'Associate Professor', 'Professor'])
+  should belong_to(:department)
+  should have_many(:assignments)
+  should have_many(:courses).through(:assignments)
 
   # Context
   context "Given context" do
     setup do 
-      # build the testing context here...
       create_departments
       create_faculties
     end
 
-    # with that testing context, write your tests...
+    should "order faculties alphabetically by last name then first name" do
+      faculties = Faculty.alphabetical
+      assert_equal ["Kaufer", "Kosbie", "Quesenberry"], faculties.map(&:last_name)
+    end
 
-    
-
+    should "return only active faculties with active scope" do
+      active_faculties = Faculty.active
+      assert_equal 2, active_faculties.count
+      assert_equal ["Kosbie", "Quesenberry"], active_faculties.alphabetical.map(&:last_name)
+    end
   end
 end
